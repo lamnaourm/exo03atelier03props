4c6167f8370305b0eab923e048de8703ebe25379
lamnaourm <mohammed.lamnaour@gmail.com>
2023-09-27 16:40:12 +0100

    structure de projet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+const livres = [
+  { id: 1, titre: 'Reminders of Him: A Novel', prix: 90.97, url: 'https://m.media-amazon.com/images/I/71rdsaOMvVL._SY522_.jpg' },
+  { id: 2, titre: 'Ugly Love: A Novel', prix: 102.5, url: 'https://m.media-amazon.com/images/I/71E8VNPC1dL._SY522_.jpg' },
+  { id: 3, titre: 'Where the Crawdads Sing', prix: 78.96, url: 'https://m.media-amazon.com/images/I/41KaqVvSGoL.jpg' },
+  { id: 4, titre: 'November 9: A Novel', prix: 123.55, url: 'https://m.media-amazon.com/images/I/51ohVcmZMpL.jpg' },
+  { id: 5, titre: 'The Return of the Gods', prix: 27.90, url: 'https://m.media-amazon.com/images/I/51vBXYM3VoL.jpg' },
+  { id: 6, titre: 'I Love You to the Moon and Back', prix: 55.85, url: 'https://m.media-amazon.com/images/I/51rW2153DLL.jpg' },
+  { id: 7, titre: 'All Good People Here: A Novel', prix: 90.34, url: 'https://m.media-amazon.com/images/I/41E1SK2D3JL.jpg' },
+  { id: 8, titre: 'The Great Reset: And the War for the World', prix: 100.4, url: 'https://m.media-amazon.com/images/I/41HTZMz9BNL.jpg' },
+  { id: 9, titre: 'Good Inside: A Guide to Becoming the Parent', prix: 106.5, url: 'https://m.media-amazon.com/images/I/41eo9TFVvYL.jpg' },
+  { id: 10, titre: 'The Butcher and The Wren: A Novel', prix: 34.78, url: 'https://m.media-amazon.com/images/I/410IBmKrs-L.jpg' }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
     </div>
   );
 }
